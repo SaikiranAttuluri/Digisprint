5168e5981b14119050e62543ff54e4da4c38d3a1
Sai Kiran Attuluri <sai.attuluri9966@gmail.com>
2021-05-26 16:24:18 +0530

    Digisprint

--- Digisprint.js
+++ Digisprint.js
@@ -1,37 +1,71 @@
 let sendMsgButton = document.getElementById("sendMsgButton");
+let myFormE1 = document.getElementById("myForm");
 
 
 const x = function(){
+
     let nameInput = document.getElementById("nameInput");
     let emailInput = document.getElementById("emailInput");
     let phoneInput = document.getElementById("phoneInput");
     let meaasgeInput = document.getElementById("meaasgeInput");
+    let nameErrMsgEl = document.getElementById("nameErrMsg");
+    let emailErrMsgEl = document.getElementById("emailErrMsg");
+    let phoneErrMsgEl = document.getElementById("phoneErrMsg");
+    let msgErrMsgEl = document.getElementById("msgErrMsg"); 
 
     var phoneno = "^[6-9][0-9]{9}$";
     var emailRegex = "^[a-zA-Z0-9.]*@[a-zA-Z]*[.com.org.in]"
-
-    if (!nameInput.value.trim() ){
-        alert("Enter valid name");
+    if(!nameInput.value.trim()){
+        nameErrMsgEl.textContent = "Required**";
+        nameInput.focus()
+        return false;
     }
+    else{
+        nameErrMsgEl.textContent = "" ;
+    }
+    
     if(!emailInput.value.trim()){
         
-        alert("Enter valid Email");
+        emailErrMsgEl.textContent = "Required**";
+        emailInput.focus();
+        return false;
+    }
+    else{
+        emailErrMsgEl.textContent = "" 
     }
     if(!phoneInput.value.trim()){
-        alert("Enter valid phone number");
+        phoneErrMsgEl.textContent = "Required**";
+        phoneInput.focus();
+        return false;
+    }
+    else{
+        phoneErrMsgEl.textContent = "";
     }
 
     if(!meaasgeInput.value.trim()){
-        alert("Enter valid message")
+        msgErrMsgEl.textContent = "Required**";
+        messageInput.focus();
+        return false;
+    }
+    else{
+        msgErrMsgEl.textContent = "";
     }
 
     if(!(phoneInput.value.match(phoneno))){
-        alert(`${phoneInput.value} is not valid phone number`)
+        phoneErrMsgEl.textContent = `${phoneInput.value} is not valid phone number`;
+        phoneInput.focus();
+        return false;
+    }else{
+        phoneErrMsgEl.textContent = "";
     }
     if(!emailInput.value.match(emailRegex)){
-        alert(`${emailInput.value} is not a valid email address `)
+        emailErrMsgEl.textContent = `${emailInput.value} is not a valid email address `;
+        emailInput.focus();
+        return false;
+    }else{
+        emailErrMsgEl.textContent = "";
     }
-}
+};
 
 const display = function(){
     let contactus=document.getElementById("sectionContactUs");
@@ -39,5 +73,6 @@ const display = function(){
     content.textContent = contactus;
 }
 
-
-
+// myFormEl.addEventListener("submit", function(event){
+//   event.preventDefault();
+// };
